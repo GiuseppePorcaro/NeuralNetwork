d35aa30d0c0bbf66b7d848c249cffa070b611ed9
Giuseppe Porcaro <porcarogiuseppe97@gmail.com>
2021-05-31 03:45:52 +0200

    visto alcune delle funzioni di attivazione

--- Parte A/funzioniAttivazione.py
+++ Parte A/funzioniAttivazione.py
@@ -8,31 +8,31 @@ def discesaDelGradiente(rete,eta,derivHidden,derivOut,derivBiasHidden,derivBiasO
 
     return rete
 
-def sigmoide(x):
+def sigmoide(x): #ok
     y = (1/(1 + np.exp(-x)))
     return y
 
-def identity(x):
+def identity(x): #ok
     y = x
     return y
 
 def sumOfSquares(y,t):
-    e = (1/2) * sum(np.power(y-t,2))
+    e = (1/2) * (np.power(y-t,2).sum())
     return e
 
-def crossEntropy(Y,T):
-    e=-sum(sum(T * np.log(Y)))
+def crossEntropy(Y,T): #ok
+    e=-(T * np.log(Y)).sum()
     return e
 
 def derivCrossEntropy(Y,T):
-    e = -np.sum(T / Y)
+    e = -(T / Y).sum()
     return e
 
-def derivSumOfSquares(y,t):
+def derivSumOfSquares(y,t): #ok
     e = y-t
     return e
 
-def derivSigmoide(x):
+def derivSigmoide(x): #ok
     z = sigmoide(x)
     y = z * (1-z)
     return y
